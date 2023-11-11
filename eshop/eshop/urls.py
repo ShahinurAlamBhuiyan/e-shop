51ef4c700f2bba26875d80ec3d725fca9e6a34e5
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-11-11 21:21:18 +0600

    add category section

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf import settings # showing images
+from django.conf.urls.static import static  # showing images
 
 from core.views import index, contact
 
@@ -9,4 +9,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact,name='contact' ),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # showing images
