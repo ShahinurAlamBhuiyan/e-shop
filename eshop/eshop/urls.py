04671fecd0d775b9122ffdac493c583d7a21eb34
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-11-12 08:18:09 +0600

    added item details page

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings # showing images
 from django.conf.urls.static import static  # showing images
 
 from core.views import index, contact
 
 urlpatterns = [
+    path('items/', include('item.urls')),
     path('', index, name='index'),
     path('contact/', contact,name='contact' ),
     path('admin/', admin.site.urls),
