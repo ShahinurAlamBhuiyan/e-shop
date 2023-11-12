090227c1c7f250608af9bc6554b06c60bd73e4aa
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2023-11-12 08:33:08 +0600

    contact page

--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -3,11 +3,8 @@ from django.urls import path, include
 from django.conf import settings # showing images
 from django.conf.urls.static import static  # showing images
 
-from core.views import index, contact
-
 urlpatterns = [
     path('items/', include('item.urls')),
-    path('', index, name='index'),
-    path('contact/', contact,name='contact' ),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # showing images
